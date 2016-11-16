4aaa8ba99e0ed62bdc38e4e362c835e16e67cebd
Anwesh43 <anweshthecool0@gmail.com>
2016-11-16 15:45:43 +0530

    Using a paragraph instead of words for porter stemmer

--- nltk_execs/nltk_porter_stemmer.py
+++ nltk_execs/nltk_porter_stemmer.py
@@ -1,7 +1,11 @@
 from nltk.stem import PorterStemmer
-def run(words):
+from nltk.tokenize import word_tokenize
+def run(paragraph):
     ps = PorterStemmer()
     stemmed_words = []
+    paragraph = paragraph.replace('.','')
+    words = word_tokenize(paragraph)
+    print words
     for word in words:
         stemmed_words.append(ps.stem(word))
     print stemmed_words
--- nltk_runner.py
+++ nltk_runner.py
@@ -1,14 +1,17 @@
 import sys
-paragraph='hey kevin! How are you? You have reduced a lot. But still you are handsome. By the way are you free tonight?'
-words = ['Running','Crying','bathing','eating','call']
+paragraph = ''
+try:
+    with open('./input.txt') as f:
+        paragraph = f.read()
+except:
+    print 'file does not exist'
+paragraph = paragraph.replace('\n','')
+print paragraph
 if(len(sys.argv) == 2):
     module=sys.argv[1]
     splittedWords = module.split("_")
 
     exec("from nltk_execs.{0} import *".format(module))
-    if splittedWords[len(splittedWords)-1] == "stemmer":
-        exec("run({0})".format(words))
-    else:
-        exec("run('{0}')".format(paragraph))
+    exec("run('{0}')".format(paragraph))
 else:
     print 'please provide extra param'
