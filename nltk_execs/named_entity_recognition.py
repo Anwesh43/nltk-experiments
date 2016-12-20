4d7ce0b5887a62aab0bfdb4f20aadd92433c82b2
Anwesh43 <anweshthecool0@gmail.com>
2016-12-20 18:57:32 +0530

    Adding binary=True for NER

--- nltk_execs/named_entity_recognition.py
+++ nltk_execs/named_entity_recognition.py
@@ -3,6 +3,6 @@ from util import *
 def run(paragraph):
     words = get_words_from_paragraph(paragraph)
     tagged_words = pos_tag(words)
-    named_entities = ne_chunk(tagged_words)
+    named_entities = ne_chunk(tagged_words,binary=True)
     print named_entities
     named_entities.draw()
